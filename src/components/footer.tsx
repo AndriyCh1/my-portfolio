88e1fe02b8dd6c45039faea2bc44c0e93058cf5e
AndriyCh1 <chekanovskiyacc@gmail.com>
2022-08-16 18:49:37 +0300

    feat: add footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="footer" style={{ textAlign: 'center', background: 'black' }}>
-      Inspired by
-      <a href="https://www.figma.com/community/file/941813185107750515">
-        https://www.figma.com/community/file/941813185107750515
-      </a>
+    <footer className="footer">
+      <span>Inspired by </span>
+      <a href="https://www.figma.com/community/file/941813185107750515">Fidelis`s template</a>
     </footer>
   );
 };
